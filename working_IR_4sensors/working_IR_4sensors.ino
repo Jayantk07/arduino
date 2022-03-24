f1ab6e816f952b5b7e9776a1e58b7c2cc55375f0
Jayant Kumar <65501335+Jayantk07@users.noreply.github.com>
2022-03-24 13:32:19 +0530

    Add files via upload

---
+++ working_IR_4sensors/working_IR_4sensors.ino
@@ -0,0 +1,90 @@
+#include <LiquidCrystal.h>
+
+#include <Wire.h>
+
+#include <LiquidCrystal_I2C.h>
+
+//const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+//LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+#include <LiquidCrystal_I2C.h>
+
+// set the LCD number of columns and rows
+int lcdColumns = 16;
+int lcdRows = 2;
+
+// set LCD address, number of columns and rows
+// if you don't know your display address, run an I2C scanner sketch
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
+
+//Define IR Sensors
+int IRSensor1 = 7;
+int IRSensor2 = 8;
+int IRSensor3 = 9;
+int IRSensor4 = 10;
+
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  // initialize LCD
+  lcd.init();
+  // turn on LCD backlight                      
+  lcd.backlight();
+
+  pinMode (IRSensor1, INPUT); // sensor pin INPUT
+  pinMode (IRSensor2, INPUT); // sensor pin INPUT
+  pinMode (IRSensor3, INPUT); // sensor pin INPUT
+  pinMode (IRSensor4, INPUT); // sensor pin INPUT
+}
+
+void loop()
+{
+  int statusSensor1 = digitalRead (IRSensor1);
+  int statusSensor2 = digitalRead (IRSensor2);
+  int statusSensor3 = digitalRead (IRSensor3);
+  int statusSensor4 = digitalRead (IRSensor4);
+
+  lcd.clear();
+  
+  if (statusSensor1 == 0){
+    Serial.println("Floor 1 Selected");
+    lcd.print("1st Floor");
+    lcd.setCursor(7,1);
+    lcd.print("Selected");
+    delay(2000);
+    lcd.clear();
+    delay(1000);
+  }
+  
+  else if (statusSensor2 == 0){
+    Serial.println("Floor 2 Selected");
+    lcd.print("2nd Floor");
+    lcd.setCursor(7,1);
+    lcd.print("Selected");
+    delay(2000);
+    lcd.clear();
+    delay(1000);
+  }
+  else if (statusSensor3 == 0){
+    Serial.println("Floor 3 Selected");
+    lcd.print("3rd Floor");
+    lcd.setCursor(7,1);
+    lcd.print("Selected");
+    delay(2000);
+    lcd.clear();
+    delay(1000);
+  }
+  else if (statusSensor4 == 0){
+    Serial.println("Floor 4 Selected");
+    lcd.print("4th Floor");
+    lcd.setCursor(7,1);
+    lcd.print("Selected");
+    delay(2000);
+    lcd.clear();
+    delay(1000);
+  }
+}
